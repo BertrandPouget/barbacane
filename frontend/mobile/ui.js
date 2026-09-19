3fad3c885499b2e1af7940aeddb62e46b89d7e57
BertrandPouget <markhillsxd@gmail.com>
2026-09-19 13:03:37 +0200

    Better yellow

--- frontend/mobile/ui.js
+++ frontend/mobile/ui.js
@@ -307,7 +307,7 @@ const Sheet = (() => {
 function spawnConfetti() {
   const box = $('confetti');
   if (!box || box.childElementCount > 0) return;
-  const colors = ['#f0ce06', '#ff9a3d', '#4a9a58', '#4a7aaa', '#9966cc', '#e04040'];
+  const colors = ['#fbc402', '#ff9a3d', '#4a9a58', '#4a7aaa', '#9966cc', '#e04040'];
   for (let i = 0; i < 34; i++) {
     const c = el('i', {
       style: `left:${Math.random() * 100}vw;` +
